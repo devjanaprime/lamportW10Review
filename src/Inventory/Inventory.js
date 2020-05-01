fd425dc73051a0ab2f1214688f1c323bcd9dcf60
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-01 13:14:23 -0500

    user input

--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import InventoryList from '../InventoryList/InventoryList';
 
 class Inventory extends Component{
+    state = {
+        currentItem: {
+            color: '',
+            description: '',
+            size: ''
+        },
+        items: []
+    } //end state
+
+    addItem = () =>{
+        console.log( 'in addItem' );
+        this.setState({
+            items: [ ...this.state.items, this.state.currentItem ]
+        }) //end set state
+    } // end addItem
+
+    handleChangeFor = ( event, property )=>{
+        // console.log( 'in handleChangeFor:', property, event.target.value );
+        this.setState({
+            currentItem: { 
+                ...this.state.currentItem,
+                [property]: event.target.value
+            }
+        }) //end set state
+    } //end handleChangeFor
+
     render(){
         return(
             <div id="Inventory">
-                <h1>Inventory.js</h1>
+                <div>
+                    <h1>Add Item</h1>
+                    <input type="text" placeholder="size" 
+                        onChange={ ( event )=>this.handleChangeFor( event, "size" ) }>
+                    </input>
+                    <input type="text" placeholder="color"
+                        onChange={ ( event )=>this.handleChangeFor( event, "color" ) }>
+                    </input>
+                    <input type="text" placeholder="description"
+                        onChange={ ( event )=>this.handleChangeFor( event, "description" ) }>
+                    </input>
+                    <button onClick={ this.addItem }>Add Item</button>
+                </div>
+                <h3>{JSON.stringify( this.state ) }</h3>
                 <InventoryList />
             </div>
         ) //end return
